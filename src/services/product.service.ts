39c6d5a91fcc4010d9529ee6925c6efe99116b56
ankita772 <santraankita772@gmail.com>
2025-04-30 21:02:09 +0530

    product fix

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,10 +1,11 @@
+import PrismaService from "@/db/prisma-service";
 import { RepositoryFactory } from "@/factory/repository.factory";
 import {
   CreateProductInterface,
   GetProductsFilterInterface,
   UpdateProductInterface,
 } from "@/interfaces/product";
-import { Product } from "@prisma/client";
+import { Product, ProductPrice } from "@prisma/client";
 
 class ProductService {
   private repositoryFactory: RepositoryFactory;
@@ -14,7 +15,32 @@ class ProductService {
   }
 
   async createProduct(data: CreateProductInterface): Promise<Product> {
-    return this.repositoryFactory.getProductRepository().createProduct(data);
+    return PrismaService.getInstance().$transaction(
+      async (tx) => {
+        const factory = new RepositoryFactory(tx);
+        const productRepo = factory.getProductRepository();
+        const productPriceRepo = factory.getProductPriceRepository();
+        const priceCategoryRepo = factory.getPriceCategoryRepository();
+
+        const product = await productRepo.createProduct(data);
+
+        const priceCategories = await priceCategoryRepo.getPriceCategoryList({
+          organizationId: data.organizationId,
+        });
+
+        const productPricesToCreate = priceCategories.map((category) => ({
+          organizationId: data.organizationId,
+          productId: product.id,
+          priceCategoryId: category.id,
+          price: 0,
+        }));
+
+        await productPriceRepo.bulkCreateProductPrice(productPricesToCreate);
+
+        return product;
+      },
+      { maxWait: 30000, timeout: 30000 }
+    );
   }
 
   async getProductsList(
